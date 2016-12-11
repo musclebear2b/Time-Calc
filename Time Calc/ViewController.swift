e7c1083fd5f0a84a6cf8ac70afa8115184099ea4
Richard Whent <richard@chunkster.net>
2016-12-11 03:33:59 +0000

    Initial Commit

---
+++ Time Calc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Time Calc
+//
+//  Created by Richard Whent on 11/12/2016.
+//  Copyright © 2016 Chunkster.net. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
